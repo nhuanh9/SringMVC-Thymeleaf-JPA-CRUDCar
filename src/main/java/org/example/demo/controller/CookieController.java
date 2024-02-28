017a476f908829b73a296d227d9aa7ea55bd89aa
nhuanh9 <chopperlouis@gmail.com>
2024-02-29 00:16:36 +0700

    done demo cookie

--- src/main/java/org/example/demo/controller/CookieController.java
+++ src/main/java/org/example/demo/controller/CookieController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.util.WebUtils;
 
@@ -12,23 +13,24 @@ import org.springframework.web.util.WebUtils;
 public class CookieController {
 
     @GetMapping("/cookie/increase")
-    public String increaseCookieValue(HttpServletRequest request, HttpServletResponse response, Model model) {
-        Cookie cookie = WebUtils.getCookie(request, "cookieValue");
-        int cookieValue = 0;
-        if (cookie != null) {
-            try {
-                cookieValue = Integer.parseInt(cookie.getValue()) + 1;
-            } catch (NumberFormatException e) {
-                cookieValue = 1;
-            }
-        } else {
+    public String increaseCookieValue(@CookieValue(name = "cookieValue", defaultValue = "0") String value, HttpServletResponse response, Model model) {
+        int cookieValue;
+        try {
+            // Cố gắng chuyển giá trị cookie từ String sang Integer và tăng nó lên
+            cookieValue = Integer.parseInt(value) + 1;
+        } catch (NumberFormatException e) {
+            // Trong trường hợp giá trị cookie không phải là một số, thiết lập cookieValue bằng 1
             cookieValue = 1;
         }
 
+        // Tạo một cookie mới và gửi nó lại cho client
         Cookie newCookie = new Cookie("cookieValue", String.valueOf(cookieValue));
-        newCookie.setMaxAge(15);
-        response.addCookie(newCookie);
+        newCookie.setMaxAge(15); // Thiết lập thời gian sống của cookie là 15 giây
+        response.addCookie(newCookie); // Thêm cookie vào response để gửi về client
+
+        // Thêm giá trị cookie vào model để có thể sử dụng nó trong view
         model.addAttribute("cookieValue", cookieValue);
-        return "/cookieView";
+        return "/cookieView"; // Trả về tên của view để hiển thị
     }
+
 }
