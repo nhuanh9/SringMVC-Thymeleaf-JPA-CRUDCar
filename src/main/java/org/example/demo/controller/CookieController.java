3fa9b7139a753df73c0bbadd5d187cfb6c082349
nhuanh9 <chopperlouis@gmail.com>
2024-02-29 00:17:29 +0700

    done demo cookie

--- src/main/java/org/example/demo/controller/CookieController.java
+++ src/main/java/org/example/demo/controller/CookieController.java
@@ -16,21 +16,15 @@ public class CookieController {
     public String increaseCookieValue(@CookieValue(name = "cookieValue", defaultValue = "0") String value, HttpServletResponse response, Model model) {
         int cookieValue;
         try {
-            // Cố gắng chuyển giá trị cookie từ String sang Integer và tăng nó lên
             cookieValue = Integer.parseInt(value) + 1;
         } catch (NumberFormatException e) {
-            // Trong trường hợp giá trị cookie không phải là một số, thiết lập cookieValue bằng 1
             cookieValue = 1;
         }
-
-        // Tạo một cookie mới và gửi nó lại cho client
         Cookie newCookie = new Cookie("cookieValue", String.valueOf(cookieValue));
-        newCookie.setMaxAge(15); // Thiết lập thời gian sống của cookie là 15 giây
-        response.addCookie(newCookie); // Thêm cookie vào response để gửi về client
-
-        // Thêm giá trị cookie vào model để có thể sử dụng nó trong view
+        newCookie.setMaxAge(15);
+        response.addCookie(newCookie);
         model.addAttribute("cookieValue", cookieValue);
-        return "/cookieView"; // Trả về tên của view để hiển thị
+        return "/cookieView";
     }
 
 }
